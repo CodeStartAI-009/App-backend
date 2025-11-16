86bf134b77bd2423821fd686b56fd74889c6243c
CodeStartAI-009 <2300090189csitelge@gmail.com>
2025-11-16 17:46:35 +0530

    Updated files

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 const app = express();
 
-// Connect Database
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Connect DB
 connectDB();
 
+// Routes
+app.use("/auth", require("./routes/auth"));
+
 app.get("/", (req, res) => {
-  res.send("Server is running and DB is connected!");
+  res.send("API Running");
 });
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
