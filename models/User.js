418d2433ee4759c75a2612dbeff15a8dbe959d45
CodeStartAI-009 <2300090189csitelge@gmail.com>
2025-12-13 10:09:16 +0530

    update

--- models/User.js
+++ models/User.js
@@ -1,3 +1,11 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const monthSummarySchema = new mongoose.Schema({
+  month: { type: String, required: true },
+  totalExpense: { type: Number, default: 0 },
+  totalIncome: { type: Number, default: 0 },
+});
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, trim: true },
@@ -39,3 +47,15 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+
+userSchema.methods.setSensitiveData = async function ({ upi, bankNumber }) {
+  if (upi) {
+    this.upiHash = await bcrypt.hash(String(upi), 10);
+  }
+  if (bankNumber) {
+    this.bankNumberHash = await bcrypt.hash(String(bankNumber), 10);
+  }
+};
+
+module.exports = mongoose.model("User", userSchema);
